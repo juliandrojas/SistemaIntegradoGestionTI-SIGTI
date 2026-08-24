ee329b68817637815c5ba91152ae8e1007ed1fe9
Julian Rojas <juliandrojasa140201@gmail.com>
2026-08-24 13:07:43 -0500

    add("Install CORS and use it")

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
+import cors from "cors";
 import express from "express";
-
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
